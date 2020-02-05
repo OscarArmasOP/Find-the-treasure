833ee621e9e6f0087751a765eb04756174f39aab
Oscar <oma_1506@hotmail.com>
2020-02-04 23:44:17 -0600

    First functional version

--- js/game.js
+++ js/game.js
@@ -8,11 +8,19 @@ let target = {
 
 let $map = document.getElementById('map');
 let $distance = document.getElementById('distance');
+let clicks = 0;
 
 $map.addEventListener('click', function(e) {
+    console.log(`click`);
+    clicks++;
     let distance = getDistance(e, target);
     let distanceHint = getDistanceHint(distance);
-    $distance.innerHTML = distanceHint;
 
+    $distance.innerHTML = `<h1>${distanceHint}</h1>`;
+
+    if (distance < 20) {
+        alert(`Found the treasure in ${clicks} clicks!`);
+        location.reload();
+    }
 
 })
